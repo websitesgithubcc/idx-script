75961873cd229fc173cddcf13c129672b6c03121
websitesgithubcc <ian@californiaconcierge.com>
2024-05-02 14:55:38 -0700

    idx.js

--- idx.js
+++ idx.js
@@ -1,6 +1,5 @@
- src="https://kestrel.idxhome.com/ihf-kestrel.js">
-	window.ihfKestrel = window.ihfKestrel || {};
-	ihfKestrel.config = {
-		platform: "custom",
-		activationToken: "22522a16-3026-4564-afbe-feae090fbfab",
-	};
+window.ihfKestrel = window.ihfKestrel || {};
+ihfKestrel.config = {
+    platform: "custom",
+    activationToken: "22522a16-3026-4564-afbe-feae090fbfab",
+};
