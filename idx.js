b64ba3375b8562fefccdd83901778dd46e174e8a
websitesgithubcc <ian@californiaconcierge.com>
2024-05-03 05:50:36 +0800

    Add files via upload

---
+++ idx.js
@@ -0,0 +1,6 @@
+ src="https://kestrel.idxhome.com/ihf-kestrel.js">
+	window.ihfKestrel = window.ihfKestrel || {};
+	ihfKestrel.config = {
+		platform: "custom",
+		activationToken: "22522a16-3026-4564-afbe-feae090fbfab",
+	};
